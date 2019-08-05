174d75a93fa9ecf2688f8945a1b7b9e337704cad
Alexander Pittendrigh <a.pittendrigh@pm.me>
2019-08-05 07:00:07 +0200

    stuff

--- src/event_sourcing/core.cljs
+++ src/event_sourcing/core.cljs
@@ -1,13 +1,26 @@
 (ns event-sourcing.core)
 
-(println "Hello world!")
+(println "Hello world!" (.now js/Date))
 
 ;; ADDED
 (defn average [a b]
   (/ (+ a b) 2.0))
 
+
+
+(def fs (js/require "fs"))
+(defn write-to-file [content filename]
+  (.writeFile fs filename content
+              (fn [err] (if err
+                          (println "error:
+" err)))))
+
+(def event-store "events.txt")
+;;this is not a map, so I cannot for now assoc onto it. the string needs to be converted to a map
 (defn record-event [event]
-  )
+  (-> event
+      (assoc event :timestamp (.now js/Date))
+      (write-to-file event-store)))
 
 (defmulti apply-event
   (fn [event] (:event-type event)))
@@ -17,27 +30,7 @@
 
 (defmethod apply-event :create-account [event] )
 
-
-;; (spit "abc.txt" "hello")
-;; const fs = require('fs')
-
-;; const content = 'Some content!'
-
-
-
-(def fs (js/require "fs"))
-
-;; fs.writeFile('/Users/joe/test.txt', content, err => {
-;;                                                      if (err) {
-;;                                                                console.error(err)
-;;                                                                return
-;;                                                                }
-;;                                                      //file written successfully
-;;                                                      })
-
-(defn write-to-file [content filename]
-  (.writeFile fs filename content
-              (fn [err] (if err
-                         (println "error: " err)))))
-
 (write-to-file "Hello, World!" "test.txt")
+
+(record-event {:event-type :create-account :data {:user "a.pitendrigh@pm.me"}})
+(record-event {:event-type :add-reading :data {:user "a.pittendrigh@pm.me" :meter-reading 1000}})
