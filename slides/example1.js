84777454b58defefd6e3c604fb1f5687a0792ce0
Alexander Pittendrigh <a.pittendrigh@pm.me>
2019-08-20 06:36:30 +0200

    more private public protected examples

--- slides/example1.js
+++ slides/example1.js
@@ -1,15 +1,17 @@
 //Example 1
 
-// var firstName = ["H", "e", "l", "l", "o"];
-var firstName = "Hello";
+let firstName = ["H", "e", "l", "l", "o"];
+let surname = ["W", "o", "r", "l", "d"];
 function fullName(fn, sn) {
+    var firstName = fn;
+    var surname = sn;
     return {
-        fullName: function () { return fn + " " + sn }
+        fullName: function () { return firstName + " " + surname },
+        setSurname: function (sn) { surname = sn;}
     }
 };
-var toFullName = fullName(firstName, "World");
-firstName = "adhkjas";
-console.log(toFullName.fullName());
-//fistName = "Henk";
-
-// (firstName, "World").fullName();
+var toFullName = fullName(firstName, surname);
+console.log(toFullName.fullName()); // H,e,l,l,o W,o,r,l,d
+// surname = ["J", "o", "z", "i", "J", "S"];
+toFullName.setSurname("JoziJS");
+console.log(toFullName.fullName()); // H,e,l,l,o JoziJS
