80b638054f79ca0216c8e6d7882620b062a72720
Alexander Pittendrigh <alexp@pop-os.localdomain>
2019-08-21 08:07:33 +0200

    Make the store actually do something

--- slides/example3.js
+++ slides/example3.js
@@ -1,27 +1,8 @@
 // listener pattern
 
-const createStore = (accounts) => {
-    return {
-        getCurrentState: (username) => (accounts[username])
-    }
-}
-const store = createStore({ "a.pittendrigh@gmail.com": {} });
-
 const notifyOfChange = (listeners, event) => {
     console.log('==> Notifying Listeners');
     listeners.forEach((listener) => {listener.notify(event, listener)})
-}
-
-const notifiedOfChange = (event, listener) => {
-    console.log('=== Notification recieved! ====');
-
-    const affectedUser = event.data.username;
-    const myUser = listener.data.username;
-    if (affectedUser === myUser) {
-        console.log('** Something I care about changed! **', store.getCurrentState(myUser));
-    }
-
-    console.log('=== Notification processed! ====');
 }
 
 const notify = (data) => (event) => {
@@ -36,9 +17,43 @@ const createListener = (username) => {
         notify: notifier
     }
 }
+const listeners = [createListener('a.pittendrigh@gmail.com')];
 
-const listeners = [createListener('a.pittendrigh@gmail.com')]
+const accountCreated = "account_created";
+const createStore = (accounts = {}) => {
+    const accountStore = accounts;
+    const eventProcessors = {};
+    eventProcessors[accountCreated] = (event, listeners) => {
+        const username = event.data.username;
+        const newAccount = { username, readings: [], events: [event] };
+        accountStore[username] = newAccount;
 
-console.log(listeners[0]);
+        notifyOfChange(listeners, event);
+    }
+    return {
+        getCurrentState: (username) => (accountStore[username]),
+        processEvent: (event, listeners) => {
+            eventProcessors[event.data.type](event, listeners);
+        }
+    }
+}
+
+const notifiedOfChange = (event, listener) => {
+    console.log('=== Notification recieved! ====');
+
+    const affectedUser = event.data.username;
+    const myUser = listener.data.username;
+    if (affectedUser === myUser) {
+        console.log('** Something I care about changed! **', store.getCurrentState(myUser));
+    }
+
+    console.log('=== Notification processed! ====');
+}
+
+//console.log(listeners[0]);
 // notifyOfChange(listeners, events);
-notifyOfChange(listeners, { data: { username: 'a.pittendrigh@gmail.com' }});
+//notifyOfChange(listeners, { data: { username: 'a.pittendrigh@gmail.com' }});
+
+const store = createStore();
+const createEvent = (type, username) => ({ data: { username, type }});
+store.processEvent(createEvent(accountCreated, 'a.pittendrigh@gmail.com'), listeners);
