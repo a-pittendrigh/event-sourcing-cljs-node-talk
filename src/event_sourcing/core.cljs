905e105010d4073282b366f52010dc8f08b1c47e
Alexander Pittendrigh <a.pittendrigh@pm.me>
2019-08-22 06:18:34 +0200

    working, finally

--- src/event_sourcing/core.cljs
+++ src/event_sourcing/core.cljs
@@ -6,6 +6,7 @@
 (def fs (js/require "fs"))
 
 (defn parse-event-log [events]
+  (prn "in  parase")
   (cljs.reader/read-string events))
 
 (defn read-event-log [filename callback]
@@ -19,16 +20,19 @@
 (defn write-to-file [event filename]
   (read-event-log filename
                   (fn [err data]
+                    (prn "here")
                     (if err
                       (prn "Something went wrong while reading the event log; " err)
-                      (let [events (parse-event-log data)
+                      (do
+                        (prn "here 2")
+                        (let [events (parse-event-log data)
                             events (conj events event)
                             events (str events)]
                         (.writeFile fs filename events
                                      (fn [err]
                                        (if err
                                          (println
-                                          "Something went wrong writing the event log: " err)))))))))
+                                          "Something went wrong writing the event log: " err))))))))))
 
 (def event-store "events.txt")
 
@@ -64,9 +68,6 @@
      (apply-event {:event-type :create-account :data {:user "a.pitendrigh@pm.me"}})
      (apply-event {:event-type :add-reading :data {:user "a.pittendrigh@pm.me" :meter-reading 1000}})))
 
-
-(prn "sdkashdk")
-
 (let [event {:hello "there"}
       events {:events [event event]}]
   ;;(str (clj->js event))
@@ -75,4 +76,22 @@
   ;;(clj->js event)
   (str events)
   (count (:events (cljs.reader/read-string (str events))))
+
+  (read-event-log event-store (fn [err data]
+                                (if err
+                                  (prn "Something wen't wrong" err)
+                                  (-> data
+                                      (prn)))))
+  
+  (.readFile fs event-store "utf8" (fn [err data]
+                                  (if err
+                                      (prn "Something wen't wrong" err)
+                                      (prn "Read the file ->" data))))
+
+  (-> {:event-type :create-account :data {:user "a.pitendrigh@pm.me"}}
+      (assoc :timestamp (.now js/Date))
+      #_(prn)
+      (write-to-file event-store))
   )
+
+(prn "Connected!")
